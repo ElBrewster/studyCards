c5b858d952003eb4f01267ad0501b77eb0a3aaa8
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-13 11:56:59 -0700

    Add logger function to express server file

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,10 +1,17 @@
-import express, {Express, Request, Response} from "express";
+import express, {Express, Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+const myLogger = function ( req: Request, res: Response, next: NextFunction) {
+  console.log("LOGGED");
+  next();
+}
+
+app.use(myLogger);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server is a go');
