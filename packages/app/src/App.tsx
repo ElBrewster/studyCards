efd7fb4244ff21a49b2c2822eb7d6a7a78536f97
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-13 14:43:55 -0700

    Add basic fetch functionality to app package to fetch data file from server

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,18 +1,29 @@
+import { useEffect } from "react";
 import ThreeByFive from "./components/threeByFive";
 import cardQuestions from "./data/cardQuestions";
 
 export default function App() {
-  const questions = cardQuestions.map((element, index) => {
+  const myQuestions = cardQuestions.map((element, index) => {
     return <ThreeByFive key={index} question={element.question} title={element.title} answer={element.answer}/>;
-  })
+  });
 
+  // const questions = cardQuestions.map((element, index) => {
+  //   return <ThreeByFive key={index} question={element.question} title={element.title} answer={element.answer}/>;
+  // });
+
+  fetch("http://localhost:8000/")
+    .then(res => res.json())
+    .then(data => data = myQuestions)
+
+    console.log({myQuestions})
   return (
     <div>
       <header>
           <h1>Three By Five Cards</h1>
       </header>
       <main>
-        {questions}
+        {/* {questions} */}
+        {myQuestions}
       </main>
       <footer>.</footer>
     </div>
