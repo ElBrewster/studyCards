db26356b013ed3ce258c5b4a73764623303574c2
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-14 13:34:46 -0700

    Add express server main file organization and public folder for future serving of static assets

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,42 +1,32 @@
+// Imports
 import express, { Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import notifier from "node-notifier";
 import errorHandler from "errorhandler";
 import cardQuestions from "./data/cardQuestions.js";
-import {Moment} from "moment";
 
+let moment = require("moment");
 dotenv.config();
 
+// Creating objects 
 const app = express();
 const port = process.env.PORT;
-let moment = require("moment");
 
+// TS utils
 interface TimeKeeping extends Request {
   time: number;
 }
-
-const errorNotification = (err: Error, str: string, req: Request) => {
-  const title = "Error in " + req.method + " " + req.url;
-  notifier.notify({title: title, message: str})
-}
-
-if (process.env.NODE_ENV === "development") {
-  app.use(errorHandler({log: errorNotification}));
-}
-
-app.use(cors());
-
+// Declarations
 const myLogger = function ( req: Request, res: Response, next: NextFunction) {
   console.log("LOGGED");
   console.log(`${req.method} ${req.url}`)
   next();
 }
-app.use(myLogger);
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  requestTime(req as TimeKeeping, res, next);
-});
+const errorNotification = (err: Error, str: string, req: Request) => {
+  const title = "Error in " + req.method + " " + req.url;
+  notifier.notify({title: title, message: str})
+}
 
 const requestTime = (req: TimeKeeping, res: Response, next: NextFunction) => {
   let now = moment()
@@ -45,6 +35,29 @@ const requestTime = (req: TimeKeeping, res: Response, next: NextFunction) => {
   next();
 }
 
+// Binding application-level middleware to an instance of app object with app.use() and app.METHOD()
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler({log: errorNotification}));
+}
+
+app.use(cors());
+app.use("/static", express.static("public"));
+app.use(myLogger);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  requestTime(req as TimeKeeping, res, next);
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+// METHODs
 app.get('/', (req: Request, res: Response) => {
   // res.send('Express + TypeScript Server is a go');
   res.send(cardQuestions);
