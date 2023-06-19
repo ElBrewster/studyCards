ddaebf04f84de2d858d0953208b0b0d5aae175f7
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-19 14:24:57 -0700

    Add form elements

--- packages/app/src/components/NewCardForm.tsx
+++ packages/app/src/components/NewCardForm.tsx
@@ -6,9 +6,16 @@ export default function NewCardForm() {
         event.preventDefault();
         console.log({event})
     }
+
+    function handleChange() {
+        console.log("change");
+    }
+
     return(
         <form onSubmit={(event) => handleSubmit(event)}>
-
+            <input type="text" placeholder="title" name="title" onChange={handleChange}/>
+            <input type="text" placeholder="question" name="question" onChange={handleChange}/>
+            <textarea placeholder="answer" name="answer" onChange={handleChange}/>
         </form>
     )
 }
