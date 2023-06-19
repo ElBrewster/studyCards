b3f35e2506a3c1296e481f3420a54d4b78ad9545
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-19 14:33:51 -0700

    Add value attributes to mirror form state handling in new card form

--- packages/app/src/components/NewCardForm.tsx
+++ packages/app/src/components/NewCardForm.tsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 
 export default function NewCardForm() {
-    const [addTitle, setAddTitle] = useState("");
-    const [addQuestion, setAddQuestion] = useState("");
-    const [addAnswer, setAddAnswer] = useState("");
-    
+    const [addTitle, setAddTitle] = useState({title: ""});
+    const [addQuestion, setAddQuestion] = useState({question: ""});
+    const [addAnswer, setAddAnswer] = useState({answer: ""});
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         console.log({event})
     }
 
-    function handleChange() {
+    function handleChange(event: React.FormEvent<HTMLFormElement>) {
+        const { name, value } = event.target;
         console.log("change");
     }
 
     return(
-        <form onSubmit={(event) => handleSubmit(event)}>
-            <input type="text" placeholder="title" name="title" onChange={handleChange} value={addTitle}/>
-            <input type="text" placeholder="question" name="question" onChange={handleChange} value={addQuestion}/>
-            <textarea placeholder="answer" name="answer" onChange={handleChange} value={addAnswer}/>
+        <form onSubmit={(onSubmit) => handleSubmit(onSubmit)}>
+            <input type="text" placeholder="title" name="title" onChange={handleChange} value={addTitle.title}/>
+            <input type="text" placeholder="question" name="question" onChange={handleChange} value={addQuestion.question}/>
+            <textarea placeholder="answer" name="answer" onChange={handleChange} value={addAnswer.answer}/>
             <button>SUBMIT</button>
         </form>
     )
