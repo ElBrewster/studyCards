a1de15f9fbb322c4173be88120bbdae546a2f22e
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-19 14:47:00 -0700

    Add input and textarea change handler functions with types

--- packages/app/src/components/NewCardForm.tsx
+++ packages/app/src/components/NewCardForm.tsx
@@ -1,5 +1,12 @@
 import { useState } from "react";
 
+type Question = {
+    question: string,
+    title: string,
+    answer: string,
+    more?: string
+}
+
 export default function NewCardForm() {
     const [addTitle, setAddTitle] = useState({title: ""});
     const [addQuestion, setAddQuestion] = useState({question: ""});
@@ -10,16 +17,23 @@ export default function NewCardForm() {
         console.log({event})
     }
 
-    function handleChange(event: React.FormEvent<HTMLFormElement>) {
-        const { name, value } = event.target;
+    function handleInput(event: React.FormEvent<HTMLInputElement>) {
+        const { name, value } = event.target as HTMLInputElement;
         console.log("change");
+        setAddQuestion(prev => ({...prev, [name]: value}));
+        setAddTitle(prev => ({...prev, [name]: value}));
     }
 
+    function handleTextArea(event: React.FormEvent<HTMLTextAreaElement>){
+        const { name, value } = event.target as HTMLTextAreaElement;
+        setAddAnswer(prev => ({...prev, [name]: value}));
+    }
+    
     return(
         <form onSubmit={(onSubmit) => handleSubmit(onSubmit)}>
-            <input type="text" placeholder="title" name="title" onChange={handleChange} value={addTitle.title}/>
-            <input type="text" placeholder="question" name="question" onChange={handleChange} value={addQuestion.question}/>
-            <textarea placeholder="answer" name="answer" onChange={handleChange} value={addAnswer.answer}/>
+            <input type="text" placeholder="title" name="title" onChange={handleInput} value={addTitle.title}/>
+            <input type="text" placeholder="question" name="question" onChange={handleInput} value={addQuestion.question}/>
+            <textarea placeholder="answer" name="answer" onChange={handleTextArea} value={addAnswer.answer}/>
             <button>SUBMIT</button>
         </form>
     )
