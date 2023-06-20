9dd91b96c499f95b2418a4ea9a3afaced38a4fdd
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-06-20 12:21:33 -0700

    Add randomizer and useState hook to track current random selection

--- packages/app/src/pages/Flash.tsx
+++ packages/app/src/pages/Flash.tsx
@@ -1,12 +1,23 @@
 import cardQuestions from "../data/cardQuestions";
 import ThreeByFive from "../components/ThreeByFive";
+import { useState } from "react";
 
-const showRandom = (cardQuestions) => {
-    let listNum = cardQuestions.length;
-    
-}
 export default function Flash(){
+    const [current, setCurrent] = useState({})
+    const calcRandom = (cardQuestions) => {
+        let listNum = cardQuestions.length;
+        console.log({listNum})
+        let cardNum = Math.floor(Math.random() * listNum);
+        let currentCard = cardQuestions.at(cardNum);
+        setCurrent(current);
+        console.log({currentCard})
+    }
+    
+    const showRandom = (cardQuestions) => {
+    }
     return(
-        <section className="flash-container">.</section>
+        <section className="flash-container">
+            <ThreeByFive />
+        </section>
     );
 }
